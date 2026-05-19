926394f14905eea17fe60e962941b8dd18f12786
katherineCelina <katherine.becerra3664@alumnos.udg.mx>
2026-05-19 00:42:36 -0600

    Update popup.js
    popup versión 4

--- extension/popup.js
+++ extension/popup.js
@@ -1,34 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const displayPagina = document.getElementById("pagina");
-    const displayResultado = document.getElementById("resultado");
-    const boton = document.getElementById("btn");
-
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs && tabs.length > 0) {
-            displayPagina.textContent = tabs[0].url;
-        } else {
-            displayPagina.textContent = "No disponible";
-        }
-    });
-
-    boton.addEventListener("click", () => {
+    
+    document.getElementById("btn").addEventListener("click", () => {
+        
         chrome.storage.local.get("ultimoAnalisis", (data) => {
-            
-            if (!data || !data.ultimoAnalisis) {
-                displayResultado.textContent = "No hay datos";
-                displayResultado.style.color = "#94a3b8";
+
+            if (!data.ultimoAnalisis) {
+                document.getElementById("resultado").textContent = "No hay datos";
+                document.getElementById("resultado").style.color = "#cbd5e1";
                 return;
             }
 
             let r = data.ultimoAnalisis;
 
+
             if (r.riesgo === "alto") {
-                displayResultado.textContent = "Sitio peligroso";
-                displayResultado.style.color = "#f87171"; // Rojo
+                document.getElementById("resultado").textContent = "Sitio peligroso";
+                document.getElementById("resultado").style.color = "#f87171";
             } else {
-                displayResultado.textContent = "Sitio seguro";
-                displayResultado.style.color = "#4ade80"; // Verde
+                document.getElementById("resultado").textContent = "Sitio seguro";
+                document.getElementById("resultado").style.color = "#4ade80";
             }
         });
     });
+
 });
