55b7c66f5e057d0c4cba50843221f65e20489d00
katherineCelina <katherine.becerra3664@alumnos.udg.mx>
2026-05-18 22:53:30 -0600

    Update popup.js
    funcionamiento del diseño de la ventana

--- extension/popup.js
+++ extension/popup.js
@@ -1,17 +1,24 @@
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+
+    if (tabs.length > 0) {
+
+        document.getElementById("pagina").textContent = tabs[0].url;
+    }
+});
+
 document.getElementById("btn").addEventListener("click", () => {
 
-  chrome.storage.local.get("ultimoAnalisis", (data) => {
+    chrome.storage.local.get("ultimoAnalisis", (data) => {
+        if (!data.ultimoAnalisis) {
+            document.getElementById("resultado").textContent = "No hay datos";
+            return;
+        }
 
-    if (!data.ultimoAnalisis) {
-      document.getElementById("resultado").textContent = "No hay datos";
-      return;
-    }
+        let r = data.ultimoAnalisis;
 
-    let r = data.ultimoAnalisis;
-
-    document.getElementById("resultado").textContent =
-      r.riesgo === "alto"
-        ? "Sitio peligroso"
-        : "Sitio seguro";
-  });
+        document.getElementById("resultado").textContent =
+            r.riesgo === "alto"
+                ? "Sitio peligroso"
+                : "Sitio seguro";
+    });
 });
